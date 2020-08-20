07e8d6ad9c70717bc9f311ba12a496adf6993283
karaage0703 <coldchicken@gmail.com>
2020-08-20 23:20:22 +0900

    Initial commit

---
+++ sample.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# reference
+# https://qiita.com/ynakayama/items/8616f4c0c6e372de9a42
+
+import sys, os
+
+def list_files(path):
+    for root, dirs, files in os.walk(path):
+        for filename in files:
+            fullname = os.path.join(root, filename)
+            print( fullname )
+
+def main(args):
+    path = args[1]
+    list_files(path)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(sys.argv)
